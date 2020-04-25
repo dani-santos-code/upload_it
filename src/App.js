1e5ff7a14f1f2b36f23c7af564ed6cddef1228ec
Dani Santos <santos.adaniele@gmail.com>
2020-04-25 17:59:39 -0400

    login page container layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
 import { GlobalStyles } from "./GlobalStyles";
+import LoginContainer from "./LoginContainer";
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
-    </>
+      <LoginContainer />
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 4fr;
+  min-height: 100vh;
+`;
 
 export default App;
---
+++ src/LoginContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+import LoginInput from "./LoginInput";
+export default function LoginContainer() {
+  return (
+    <>
+      <LoginInput />
+      <div>Content</div>
+    </>
+  );
+}
